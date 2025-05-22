036d7c26445b169a48888fec48877cd9be2e117c
User <user@example.com>
2025-05-22 23:41:30 +0800

    添加NFT质押功能和相关配置文件

--- deploy-package/vite.config.js
+++ deploy-package/vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/alvey-nft-staking/',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
     minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
   },
   server: {
     port: 3000,
