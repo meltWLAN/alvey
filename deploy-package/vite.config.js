7c1ecc3edbe67b31aa0e742f8a7a881897784fa3
meltWLAN <114678609+meltWLAN@users.noreply.github.com>
2025-05-23 00:09:22 +0800

    Update vite.config.js

--- deploy-package/vite.config.js
+++ deploy-package/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/alvey-nft-staking/',
+  base: base: '/alvey/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
